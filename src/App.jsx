84946f3ad782c22d1d58f36324262cae9f92462e
Hoanganhpro1 <nhatbedebucu@gmail.com>
2026-02-24 12:32:10 +0700

    them signin va signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Toaster } from "sonner"
 import { BrowserRouter, Routes, Route } from "react-router"
 import Homepage from "./pages/Homepage"
 import NotFound from "./pages/NotFound"
+import SignIn from "./pages/signIn"
+import SignUp from "./pages/SignUp"
 function App() {
 
   return (
@@ -17,7 +19,14 @@ function App() {
           <Route path="*"
             element={<NotFound />}
           />
+          <Route path="/SignIn"
+            element={<SignIn/>}
+          />
+          <Route path="/SignIn"
+            element={<SignUp/>}
+          />
         </Routes>
+
       </BrowserRouter>
     </>
   )
